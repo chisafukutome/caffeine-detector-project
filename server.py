067c199f919cb8e1cabde60fc02c89877948a10f
chisa-f <kiki4537fer2cat@gmail.com>
2022-01-30 01:08:36 -0500

    Some small fixes on frontend.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 from nutrients import Nutrition_Information
 
@@ -12,19 +12,16 @@ def create_nutrition_obj(query):
 def home():
     return render_template("caffeine.html")
 
-@app.route("/receipt")
+@app.route("/receipt", methods=['POST'])
 def receipt():
+    query = str(request.form['search_by_text'])
     today = datetime.now().strftime("%Y/%m/%d")
-    my_nutrition = create_nutrition_obj("8 oz tea")
+    my_nutrition = create_nutrition_obj(query)
     formatted_caffeine = round(my_nutrition.caffeine_amt, 2)
     amt_you_can_drink = round(400 - my_nutrition.caffeine_amt, 2)
     return render_template("receipt.html", today=today, food=my_nutrition.food, serving_quantity=my_nutrition.qty,
                            serving_unit=my_nutrition.serving_unit, caffeine_amount=formatted_caffeine,
                            amt_you_can_drink=amt_you_can_drink)
-
-@app.route("/nutrition-data")
-def nutrition_data():
-    return render_template("nutrition-data.html")
 
 
 if __name__ == "__main__":
